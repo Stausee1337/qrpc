19460af65995eb49c7765c4f1d45cdab390c9d93
Simon König <s.koenig1337@gmail.com>
2025-10-17 12:41:14 +0200

    [qrpc-js]: qrpc service runtime

--- packages/qrpc-js/src/index.ts
+++ packages/qrpc-js/src/index.ts
@@ -1,43 +1,7 @@
-import * as $s from './schemas.js'
+import * as schemas from './schemas.js'
+import * as runtime from './runtime.js'
 
-const CommonGender = $s.Enum(
-    "CommonGender",
-    "MALE",
-    "FEMALE",
-    "NONBINARY"
-);
+export { schemas, runtime }
 
-const User = $s.Record(
-    "User",
-    {
-        uuid: $s.UUID,
-        username: $s.String,
-        verified: $s.Boolean,
-        gender: $s.Union(CommonGender, $s.Number)
-    }
-);
-type User = $s.TypeOf<typeof User>;
-
-const Division = $s.Record(
-    "Division",
-    {
-        name: $s.String,
-        description: $s.String,
-        previousScores: $s.Array($s.UInt),
-    }
-);
-type Division = $s.TypeOf<typeof Division>;
-
-const Script = $s.Record(
-    "Script",
-    {
-        uuid: $s.UUID,
-        name: $s.String,
-        divisions: $s.Array(Division)
-    }
-);
-type Script = $s.TypeOf<typeof Script>;
-
-function x(user: User) {
-}
+export { createSimpleExecutor } from './runtime.js'
 
---
+++ packages/qrpc-js/src/runtime.ts
@@ -0,0 +1,141 @@
+import type { QSchema, TypeOf } from "./schemas.js";
+import { Record as createRecord } from './user-defined.js';
+
+type MapRecord<T extends Record<string, QSchema<any>>> = {
+    [P in keyof T]: TypeOf<T[P]>;
+};
+
+interface Operation<I extends Record<string, QSchema<any>>, R extends QSchema<any>> {
+    readonly kind: "mutation"|"query";
+    (input: MapRecord<I>): Promise<TypeOf<R>>;
+}
+
+interface VoidOperation<I extends Record<string, QSchema<any>>> {
+    readonly kind: "mutation";
+    (input: MapRecord<I>): Promise<void>;
+}
+
+type Executor = (url: string, body: any) => Promise<Response>;
+type Constructor<T> = new(executor: Executor) => T;
+
+function overlayPrototype<T, P>(f: Constructor<T>, p: P): Constructor<T & P> {
+    const result: Constructor<T & P> = f as any;
+    result.prototype = Object.assign(result.prototype, p)
+    return result;
+}
+
+export function service<O extends Record<string, Operation<any, any>>>(
+    name: string,
+    operations: O
+): Constructor<O> {
+    const $service: Constructor<$Service> = class extends $Service {
+        constructor(executor: Executor) {
+            super(name, executor);
+        }
+    };
+
+    return overlayPrototype($service, operations)
+}
+
+function buildOperation<I extends Record<string, QSchema<any>>, R extends QSchema<any>>(
+    kind: "mutation",
+    name: string,
+    inputTypes: I,
+): VoidOperation<I>
+function buildOperation<I extends Record<string, QSchema<any>>, R extends QSchema<any>>(
+    kind: "mutation"|"query",
+    name: string,
+    inputTypes: I,
+    outputSchema: R
+): Operation<I, R>
+function buildOperation<I extends Record<string, QSchema<any>>, R extends QSchema<any>>(
+    kind: "mutation"|"query",
+    name: string,
+    inputTypes: I,
+    outputSchema?: R
+): Operation<I, R>|VoidOperation<I> {
+    const inputSchema = createRecord(`${name}.Inputs`, inputTypes);
+    async function executeOp(this: $Service|unknown, input: MapRecord<I>): Promise<TypeOf<R>> {
+        if (!(this instanceof $Service))
+            throw ''
+        const rawInput = inputSchema.serialize(input);
+        const rawOutput = this.$executeOperation(kind, name, rawInput)
+        if (outputSchema === undefined)
+            return;
+        return outputSchema.parse(rawOutput)
+    }
+    executeOp.kind = kind;
+
+    return executeOp;
+}
+
+export function query<I extends Record<string, QSchema<any>>, R extends QSchema<any>>(
+    name: string,
+    inputTypes: I,
+    outputSchema: R
+): Operation<I, R> {
+    return buildOperation("query", name, inputTypes, outputSchema)
+}
+
+export function mutation<I extends Record<string, QSchema<any>>>(
+    name: string,
+    inputTypes: I,
+): VoidOperation<I>;
+export function mutation<I extends Record<string, QSchema<any>>, R extends QSchema<any>>(
+    name: string,
+    inputTypes: I,
+    outputSchema: R
+): Operation<I, R>
+export function mutation<I extends Record<string, QSchema<any>>, R extends QSchema<any>>(
+    name: string,
+    inputTypes: I,
+    outputSchema?: R
+): Operation<I, R>|VoidOperation<I> {
+    if (outputSchema === undefined)
+        return buildOperation("mutation", name, inputTypes)
+    return buildOperation("mutation", name, inputTypes, outputSchema)
+}
+
+abstract class $Service {
+    public $executor: Executor;
+    constructor(public $name: string, executor: Executor) {
+        this.$executor = executor;
+    }
+
+    async $executeOperation(
+        kind: "mutation"|"query",
+        name: string,
+        input: any
+    ): Promise<unknown> {
+        const response = await this.$executor(`/${kind}:${this.$name}.${name}`, input);
+        const [data, error] = await this.#parseResponse(response);
+        if (error !== undefined)
+            throw `Unexpected API Response: ${error}`
+        return data;
+    }
+
+    async #parseResponse(response: Response): Promise<[unknown, undefined]|[undefined, string]> {
+        if (response.status !== 200)
+            return [undefined, `${response.status}: ${response.statusText}`]
+        const rawJSON = await response.json();
+        if (typeof rawJSON !== "object") 
+            return [undefined, `unexpected ${typeof rawJSON}`]
+        if (rawJSON === null)
+            return [undefined, `unexpected null`]
+        const rawData = Object.getOwnPropertyDescriptor(rawJSON, 'data');
+        if (rawData !== undefined)
+            return [rawData.value, undefined];
+        const rawError = Object.getOwnPropertyDescriptor(rawJSON, 'error');
+        if (rawError !== undefined)
+            return [undefined, String(rawError.value)];
+        return [undefined, undefined]
+    }
+}
+
+export function createSimpleExecutor(baseURL: string): Executor {
+    return async (endpoint: string, body: any): Promise<Response> => {
+        return await fetch(`${baseURL}/${endpoint}`, { method: "POST", body })
+    }
+}
+
+
